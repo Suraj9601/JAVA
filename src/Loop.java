0877eaf128736a256d585e39a555b2ae2dc06bf1
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-10 19:29:26 +0530

    Created Temperature.java

---
+++ src/Loop.java
@@ -0,0 +1,17 @@
+public class Loop {
+    public static void main(String[] args) {
+
+        // while loop
+//        int count = 1;
+//        while (count != 5) {
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // for loop
+        for (int i=1; i != 5; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
