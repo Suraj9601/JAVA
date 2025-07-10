21d7afd1c22d612fb1c1438665664a0af5158796
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-10 19:05:21 +0530

    Type casting files

---
+++ src/TypeCasting.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        int num = (int)(33.65f);
+        System.out.println(num);
+
+        int a = 257;
+        byte b = (byte)(a);
+        System.out.println(b);
+    }
+}
