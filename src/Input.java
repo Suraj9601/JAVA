8482a04a44e056f6290940c044c8b5dd71011d0d
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-10 18:00:23 +0530

    .

---
+++ src/Input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter your full name : ");
+        String name = input.nextLine();
+        System.out.print("Please Enter your roll no : ");
+        int rollNo = input.nextInt();
+        System.out.println("Your name is " + name);
+        System.out.println("Your roll number is " + rollNo);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
-        
+
+        // Primitive Datatype
+        int rollNo = 101;  // size = 4 bytes
+        char division = 'A';
+        float marks = 91.68f; // size = 4 bytes.
+        double largeDecimalNo = 738882393.83873662;  //size 8 bytes
+        long largeIntValues = 649297663772838L;  //size 8 bytes
+        boolean check = true;
+
+        // Non-Primitive Datatype
+        String name = "Suraj";
+
+        int a = 234000000;
+        System.out.println(a);
     }
 }
