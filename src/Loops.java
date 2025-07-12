183845dd6e44cdc877746743cc4bbaed662f9114
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-12 12:27:30 +0530

    Update Loops.java

--- src/Loops.java
+++ src/Loops.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
 
-         // while loop
-        int count = 1;
-        while (count != 5) {
-            System.out.println(count);
-            count++;
+        // Q : Print numbers 1 to 10
+
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println(i);
         }
 
-        // for loop
-        for (int i=1; i != 5; i++) {
+        // Q : print number 1 to n
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = input.nextInt();
+
+        for (int i = 1; i <= num; i++) {
             System.out.println(i);
         }
     }
