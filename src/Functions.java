803e833a7114922010d446708c3f9f414c8cda3f
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-12 12:35:21 +0530

    Update Functions.java

--- src/Functions.java
+++ src/Functions.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Functions {
+    public static void main(String[] args) {
+
+        // Q : Take input of 2 numbers and print sum
+
+        Scanner input = new Scanner(System.in);
+        int num1,num2,sum;
+
+        System.out.print("Enter the First number : ");
+         num1 = input.nextInt();
+
+        System.out.print("Enter Second number : ");
+         num2 = input.nextInt();
+
+         sum = num1 + num2;
+
+        System.out.println(sum);
+    }
 }
