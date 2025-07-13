66a99f53a2940f262aae840afeb6ba69855287fc
Suraj Aglave <aglavesuraj96@gmail.com>
2025-07-13 12:52:04 +0530

    Update Array.java

--- src/Array.java
+++ src/Array.java
@@ -8,16 +8,20 @@ public class Array {
         // datatype[] variable_name = new datatype[size];
 
         Scanner input = new Scanner(System.in);
-
+        // integer array
         int[] arr = new int[5];
-        arr[0] = 1;
+        System.out.println("Enter Five Integer : ");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = input.nextInt();
         }
         System.out.println(Arrays.toString(arr));
-//        for (int i : arr) {
-//            System.out.print(i + " ");
-//        }
 
+        // String Array
+        System.out.println("Enter Five String : ");
+        String[] str = new String[5];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = input.next();
+        }
+        System.out.println(Arrays.toString(str));
     }
 }
